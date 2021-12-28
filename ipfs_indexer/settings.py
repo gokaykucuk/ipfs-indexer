8fb4011118ff102a03c3e1e202fc97312e994bc2
Gokay Kucuk <gokay@hey.com>
2021-12-28 13:14:41 +0100

    staticfiles

--- ipfs_indexer/settings.py
+++ ipfs_indexer/settings.py
@@ -118,7 +118,7 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
